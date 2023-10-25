39d614467dccea2ecbd44779d347dae813f644f0
roodolfopc <rodolfopontesc@outlook.com>
2023-10-25 17:42:00 -0300

    Docker file deploy

---
+++ src/main/java/br/com/roodolfopc/todolist/user/UserController.java
@@ -0,0 +1,58 @@
+package br.com.roodolfopc.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+/**
+ * Modificadores:
+ * public
+ * private
+ * protected
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+  @Autowired
+  private IUserRepository userRepository;
+
+  /**
+   * Tipos de classes:
+   * String (texto)
+   * Integer (int) Números inteiros
+   * Double (double) Números 0.0000
+   * Float (float) Números 0.000
+   * char (A C )
+   * Date (data)
+   * void
+   */
+
+  /**
+   * Body
+   * 
+   */
+  @PostMapping("/")
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if (user != null) {
+      // Mensagem de Erro
+      // Status Code
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+    }
+
+    var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+    userModel.setPassword(passwordHashred);
+
+    var userCreated = this.userRepository.save(userModel);
+    return ResponseEntity.status(HttpStatus.OK).body(userCreated);
+  }
+}
